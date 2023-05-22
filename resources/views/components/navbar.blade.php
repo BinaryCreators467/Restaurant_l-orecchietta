9c9f1cc63206a67fcaa0cb2573a09e0b927e0a09
carmine strollo <carmine.987@hotmail.it>
2023-05-22 18:07:02 +0200

    bastardo responsive

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 
-<nav class="navbar navbar-expand-lg bg-danger">
+<nav class="navbar navbar-expand-lg bg shadow">
     <div class="container-fluid">
       <a class="navbar-brand" href="{{route('home')}}">Home</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,9 @@
             <div class="col-12">
 
 
-                <h1 class="ms-5 mt-5">L'ORECCHIETTA</h1>
-                <p class="ms-5 mt-3">cucina tipica barese</p>
-                {{-- <img src="/storage/images/restaurant.jpg" alt="restaurant" height="500px" width="100%"> --}}
+                <h3 class="titleCover bg-primary">L'ORECCHIETTA</h3>
+                <h3 class="subTcover">cucina tipica barese</h3>
+                
 
 
 
